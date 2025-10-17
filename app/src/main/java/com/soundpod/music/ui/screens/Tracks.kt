4bf409ab80f4394991601abe658ec90b7a7f8693
arunnechully <arunnechully2003@gmail.com>
2025-10-17 08:05:23 +0530

    v1.0 ui improvements

---
+++ app/src/main/java/com/soundpod/music/ui/screens/Tracks.kt
@@ -0,0 +1,47 @@
+package com.soundpod.music.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.soundpod.music.data.MusicRepository
+import com.soundpod.music.data.Song
+import com.soundpod.music.ui.components.SongItem
+import com.soundpod.music.utils.RequestAudioPermission
+
+@Composable
+fun Tracks (){
+
+    val context = LocalContext.current
+    var songs by remember { mutableStateOf<List<Song>>(emptyList()) }
+    RequestAudioPermission {
+        songs = MusicRepository.getAllSongs(context)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        if (songs.isEmpty()) {
+            Text(
+                text = "No songs found or permission denied."
+            )
+        } else {
+            LazyColumn {
+                items(songs) { song ->
+                    SongItem(song = song, songs = songs, context = context)
+                }
+            }
+        }
+    }
+}
