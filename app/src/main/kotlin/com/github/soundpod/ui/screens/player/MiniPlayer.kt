a50c4d75e2de96cabfaa870b5f172fbd4a17df9a
arunnechully <arunnechully2003@gmail.com>
2025-11-18 19:28:42 +0530

    updated to v1.0.3

--- app/src/main/kotlin/com/github/soundpod/ui/screens/player/MiniPlayer.kt
+++ app/src/main/kotlin/com/github/soundpod/ui/screens/player/MiniPlayer.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -150,7 +151,7 @@ fun MiniPlayer(
                     }
                 },
                 colors = ListItemDefaults.colors(
-                    containerColor = BottomSheetDefaults.ContainerColor
+                    containerColor = Color.Transparent
                 )
             )
 
