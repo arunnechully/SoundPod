825d6fb3c1a12fb6dbcd2e00df0fa9ee6c8b11c9
arunnechully <194563389+arunnechully@users.noreply.github.com>
2026-05-31 16:24:18 +0530

    refactor: update settings UI styles and relocate NotificationDismissReceiver
    - Relocate `NotificationDismissReceiver` from an inner class of `PlayerService` to a standalone class.
    - Update `SettingsScreenLayout` to use trailing lambda syntax for improved readability.
    - Adjust `SettingsScreenLayout` shapes: changed to `extraLarge` in `AppearanceSettings` and removed explicit `extraSmall` assignment in `PlayerSettings`.

--- app/src/main/kotlin/com/github/soundpod/ui/screens/settings/AppearanceSettings.kt
+++ app/src/main/kotlin/com/github/soundpod/ui/screens/settings/AppearanceSettings.kt
@@ -35,48 +35,47 @@ fun AppearanceSettings(
 
     SettingsScreenLayout(
         title = stringResource(id = R.string.appearance),
-        shape = MaterialTheme.shapes.extraSmall,
+        shape = MaterialTheme.shapes.extraLarge,
         onBackClick = onBackClick,
-        content = {
-            SettingsGroup(
-                title = stringResource(id = R.string.theme),
-            ) {
-                EnumValueSelectorSettingsEntry(
-                    title = stringResource(id = R.string.app_theme),
-                    selectedValue = appThemeColor,
-                    onValueSelected = { appThemeColor = it },
-                    icon = IconSource.Icon( painterResource(id = R.drawable.dark_mode)),
-                    valueText = { stringResource(it.resourceId) }
-                )
-            }
-
-            SettingsGroup(
-                title = stringResource(id = R.string.player_style),
-            ) {
-                EnumValueSelectorSettingsEntry(
-                    title = stringResource(id = R.string.player_layout),
-                    selectedValue = playerlayout,
-                    onValueSelected = { playerlayout = it },
-                    icon = IconSource.Icon( painterResource(id = R.drawable.layout)),
-                    valueText = { stringResource(it.resourceId) }
-                )
-            }
-
-            SettingsGroup{
-                EnumValueSelectorSettingsEntry(
-                    title = stringResource(id = R.string.progress_bar_style),
-                    selectedValue = progressBarStyle,
-                    onValueSelected = { progressBarStyle = it },
-                    icon = IconSource.Icon( painterResource(id = R.drawable.wave)),
-                    valueText = { stringResource(it.resourceId) }
-                )
-                SettingColumn(
-                    icon = IconSource.Vector(Icons.Default.BlurOn),
-                    title = stringResource(id = R.string.background_style),
-                    description = stringResource(id = R.string.background_style_discription),
-                    onClick = onBackgroundClick
-                )
-            }
+    ){
+        SettingsGroup(
+            title = stringResource(id = R.string.theme),
+        ) {
+            EnumValueSelectorSettingsEntry(
+                title = stringResource(id = R.string.app_theme),
+                selectedValue = appThemeColor,
+                onValueSelected = { appThemeColor = it },
+                icon = IconSource.Icon( painterResource(id = R.drawable.dark_mode)),
+                valueText = { stringResource(it.resourceId) }
+            )
         }
-    )
+
+        SettingsGroup(
+            title = stringResource(id = R.string.player_style),
+        ) {
+            EnumValueSelectorSettingsEntry(
+                title = stringResource(id = R.string.player_layout),
+                selectedValue = playerlayout,
+                onValueSelected = { playerlayout = it },
+                icon = IconSource.Icon( painterResource(id = R.drawable.layout)),
+                valueText = { stringResource(it.resourceId) }
+            )
+        }
+
+        SettingsGroup{
+            EnumValueSelectorSettingsEntry(
+                title = stringResource(id = R.string.progress_bar_style),
+                selectedValue = progressBarStyle,
+                onValueSelected = { progressBarStyle = it },
+                icon = IconSource.Icon( painterResource(id = R.drawable.wave)),
+                valueText = { stringResource(it.resourceId) }
+            )
+            SettingColumn(
+                icon = IconSource.Vector(Icons.Default.BlurOn),
+                title = stringResource(id = R.string.background_style),
+                description = stringResource(id = R.string.background_style_discription),
+                onClick = onBackgroundClick
+            )
+        }
+    }
 }
--- app/src/main/kotlin/com/github/soundpod/ui/screens/settings/PlayerSettings.kt
+++ app/src/main/kotlin/com/github/soundpod/ui/screens/settings/PlayerSettings.kt
@@ -3,13 +3,12 @@ package com.github.soundpod.ui.screens.settings
 import androidx.activity.compose.BackHandler
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.VolumeUp
+import androidx.compose.material.icons.automirrored.outlined.ExitToApp
 import androidx.compose.material.icons.automirrored.outlined.QueueMusic
 import androidx.compose.material.icons.filled.MusicOff
-import androidx.compose.material.icons.automirrored.outlined.ExitToApp
 import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.Timer
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -57,115 +56,111 @@ fun PlayerSettings(
 
     SettingsScreenLayout(
         title = stringResource(id = R.string.player),
-        shape = MaterialTheme.shapes.extraSmall,
         onBackClick = onBackClick,
-        content = {
+    ) {
+        SettingsGroup(
+            title = stringResource(R.string.play_back)
+        ) {
+            SwitchSetting(
+                icon = IconSource.Vector(Icons.AutoMirrored.Outlined.QueueMusic),
+                title = stringResource(id = R.string.persistent_queue),
+                description = stringResource(id = R.string.persistent_queue_description),
+                switchState = persistentQueue,
+                onSwitchChange = {
+                    persistentQueue = it
+                }
+            )
 
-            SettingsGroup(
-                title = stringResource(R.string.play_back)
-            ) {
-                SwitchSetting(
-                    icon = IconSource.Vector(Icons.AutoMirrored.Outlined.QueueMusic),
-                    title = stringResource(id = R.string.persistent_queue),
-                    description = stringResource(id = R.string.persistent_queue_description),
-                    switchState = persistentQueue,
-                    onSwitchChange = {
-                        persistentQueue = it
-                    }
-                )
+            SwitchSetting(
+                icon = IconSource.Vector(Icons.Outlined.Timer),
+                title = stringResource(R.string.stop_after_current),
+                description = stringResource(R.string.stop_after_current_description),
+                switchState = stopAfterCurrent,
+                onSwitchChange = {
+                    stopAfterCurrent = it
+                }
+            )
 
-                SwitchSetting(
-                    icon = IconSource.Vector(Icons.Outlined.Timer),
-                    title = stringResource(R.string.stop_after_current),
-                    description = stringResource(R.string.stop_after_current_description),
-                    switchState = stopAfterCurrent,
-                    onSwitchChange = {
-                        stopAfterCurrent = it
-                    }
-                )
+            SwitchSetting(
+                icon = IconSource.Vector(Icons.Default.MusicOff),
+                title = stringResource(id = R.string.skip_silence),
+                description = stringResource(id = R.string.skip_silence_description),
+                switchState = skipSilence,
+                onSwitchChange = {
+                    skipSilence = it
+                }
+            )
+            SwitchSetting(
+                icon = IconSource.Icon(painterResource(R.drawable.headphone)),
+                title = stringResource(id = R.string.resume_playback),
+                description = stringResource(id = R.string.resume_playback_description),
+                switchState = resumePlaybackWhenDeviceConnected,
+                onSwitchChange = {
+                    resumePlaybackWhenDeviceConnected = it
+                }
+            )
 
-                SwitchSetting(
-                    icon = IconSource.Vector(Icons.Default.MusicOff),
-                    title = stringResource(id = R.string.skip_silence),
-                    description = stringResource(id = R.string.skip_silence_description),
-                    switchState = skipSilence,
-                    onSwitchChange = {
-                        skipSilence = it
-                    }
-                )
-                SwitchSetting(
-                    icon = IconSource.Icon(painterResource(R.drawable.headphone)),
-                    title = stringResource(id = R.string.resume_playback),
-                    description = stringResource(id = R.string.resume_playback_description),
-                    switchState = resumePlaybackWhenDeviceConnected,
-                    onSwitchChange = {
-                        resumePlaybackWhenDeviceConnected = it
-                    }
-                )
-                
-                SwitchSetting(
-                    icon = IconSource.Vector(Icons.AutoMirrored.Outlined.ExitToApp),
-                    title = stringResource(R.string.stop_on_app_close),
-                    description = stringResource(R.string.stop_on_app_close_description),
-                    switchState = pauseOnAppClose,
-                    onSwitchChange = {
-                        pauseOnAppClose = it
-                    }
-                )
-            }
-
-            SettingsGroup(
-                title = stringResource(R.string.audio)
-            ) {
-                SwitchSetting(
-                    icon = IconSource.Vector(Icons.AutoMirrored.Filled.VolumeUp),
-                    title = stringResource(id = R.string.loudness_normalization),
-                    description = stringResource(id = R.string.loudness_normalization_description),
-                    switchState = volumeNormalization,
-                    onSwitchChange = {
-                        volumeNormalization = it
-                    }
-                )
-            }
-
-            SettingsGroup(
-                title = stringResource(R.string.lockscreen)
-            ) {
-                SwitchSetting(
-                    icon = IconSource.Vector(Icons.Outlined.Image),
-                    title = stringResource(id = R.string.show_song_cover),
-                    description = stringResource(id = R.string.show_song_cover_description),
-                    switchState = isShowingThumbnailInLockscreen,
-                    onSwitchChange = { isShowingThumbnailInLockscreen = it }
-                )
-            }
-
-            SettingsGroup(
-                title = stringResource(R.string.advanced)
-            ) {
-                SliderSettingsItem(
-                    label = stringResource(R.string.play_back) + " speed",
-                    value = playSpeed,
-                    onValueChange = { playSpeed = it },
-                    valueRange = 0.5f..2.0f,
-                    valueLabel = { String.format(Locale.US, "%.1fx", it) },
-                    hapticUseIntegerStep = false,
-                    hapticUseFloatStep = true,
-                    hapticFloatStep = 0.1f
-                )
-
-                SliderSettingsItem(
-                    label = stringResource(R.string.play_pitch),
-                    value = playPitch,
-                    onValueChange = { playPitch = it },
-                    valueRange = 0.5f..2.0f,
-                    valueLabel = { String.format(Locale.US, "%.1fx", it) },
-                    hapticUseIntegerStep = false,
-                    hapticUseFloatStep = true,
-                    hapticFloatStep = 0.1f
-                )
-            }
-                
+            SwitchSetting(
+                icon = IconSource.Vector(Icons.AutoMirrored.Outlined.ExitToApp),
+                title = stringResource(R.string.stop_on_app_close),
+                description = stringResource(R.string.stop_on_app_close_description),
+                switchState = pauseOnAppClose,
+                onSwitchChange = {
+                    pauseOnAppClose = it
+                }
+            )
         }
-    )
+
+        SettingsGroup(
+            title = stringResource(R.string.audio)
+        ) {
+            SwitchSetting(
+                icon = IconSource.Vector(Icons.AutoMirrored.Filled.VolumeUp),
+                title = stringResource(id = R.string.loudness_normalization),
+                description = stringResource(id = R.string.loudness_normalization_description),
+                switchState = volumeNormalization,
+                onSwitchChange = {
+                    volumeNormalization = it
+                }
+            )
+        }
+
+        SettingsGroup(
+            title = stringResource(R.string.lockscreen)
+        ) {
+            SwitchSetting(
+                icon = IconSource.Vector(Icons.Outlined.Image),
+                title = stringResource(id = R.string.show_song_cover),
+                description = stringResource(id = R.string.show_song_cover_description),
+                switchState = isShowingThumbnailInLockscreen,
+                onSwitchChange = { isShowingThumbnailInLockscreen = it }
+            )
+        }
+
+        SettingsGroup(
+            title = stringResource(R.string.advanced)
+        ) {
+            SliderSettingsItem(
+                label = stringResource(R.string.play_back) + " speed",
+                value = playSpeed,
+                onValueChange = { playSpeed = it },
+                valueRange = 0.5f..2.0f,
+                valueLabel = { String.format(Locale.US, "%.1fx", it) },
+                hapticUseIntegerStep = false,
+                hapticUseFloatStep = true,
+                hapticFloatStep = 0.1f
+            )
+
+            SliderSettingsItem(
+                label = stringResource(R.string.play_pitch),
+                value = playPitch,
+                onValueChange = { playPitch = it },
+                valueRange = 0.5f..2.0f,
+                valueLabel = { String.format(Locale.US, "%.1fx", it) },
+                hapticUseIntegerStep = false,
+                hapticUseFloatStep = true,
+                hapticFloatStep = 0.1f
+            )
+        }
+    }
 }
